3f93d952ee89689f7321e77bffe8b7c3ad89b848
Vytis <vytisjons@gmail.com>
2020-01-02 20:25:43 +0200

    Add project files.

---
+++ Todo/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Todo/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public Group Group { get; set; }
+
+
+    }
+}
